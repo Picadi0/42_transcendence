ec66728dabd433000ebd8fc20c9966030b4a634b
maltun <melihmelihaltun@outlook.com>
2025-01-31 13:38:17 +0300

    rejoins automatic when tries host/join and reworked on websocket

--- srcs/backend/game/consumers.py
+++ srcs/backend/game/consumers.py
@@ -12,29 +12,30 @@ logger = logging.getLogger(__name__)
 
 class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        logger.error("Websocket bağlanıyor")
+        logger.error("Websocket bağlaniyor")
         self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
         self.user_id = self.scope["url_route"]["kwargs"]["user_id"]
         self.group_name = self.room_id
 
-        # Kullanıcıyı gruba ekle
+        # Kullaniciyi gruba ekle
         await self.channel_layer.group_add(self.group_name, self.channel_name)
-
+        userCanJoinRoom = False
         try:
             # Fetch all rooms and evaluate the QuerySet synchronously
             rooms = await sync_to_async(list)(TMPGameDB.objects.all())
 
-            self.isHost = False
-
             # Check if user is in any room (player1 or player2) using in-memory list
-            userCanJoinRoom = any(
-                room.player1_id == self.user_id or room.player2_id == self.user_id
-                for room in rooms
-            )
-
-            # Determine if the user is a host
-            if userCanJoinRoom:
-                self.isHost = any(room.player1_id == self.user_id for room in rooms)
+            for room in rooms:
+                if room.room_id == self.room_id:
+                    if room.player1_id == self.user_id:
+                        self.isHost = True
+                        userCanJoinRoom = True
+                    elif room.player2_id == self.user_id:
+                        self.isHost = False
+                        userCanJoinRoom = True
+                    else:
+                        userCanJoinRoom = False
+                    break
 
         except Exception as e:
             logger.error(f"game/Consumer.py Error {e}")
@@ -43,14 +44,14 @@ class GameConsumer(AsyncWebsocketConsumer):
 
         if userCanJoinRoom:
             await self.accept()
-            logger.error(f"{self.user_id} şu oyuna {self.room_id} katıldı")
+            logger.error(f"{self.user_id} şu oyuna {self.room_id} katildi")
         else:
             logger.error(
-                f"error: {self.user_id} şu oyuna {self.room_id} katılamadı çünkü gameDb'de bulunamadı"
+                f"error: {self.user_id} şu oyuna {self.room_id} katilamadi çünkü gameDb'de bulunamadi"
             )
             await self.close()
 
-    # Kullanıcıyı gruptan çıkar
+    # Kullaniciyi gruptan çikar
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
@@ -67,7 +68,7 @@ class GameConsumer(AsyncWebsocketConsumer):
                 elif self.user_id == room.player2_id:
                     remaining_player_id = room.player1_id
                 else:
-                    logger.error(f"Kullanıcı {self.user_id} bu odada bulunamadı.")
+                    logger.error(f"Kullanici {self.user_id} bu odada bulunamadi.")
                     return
 
                 # If there's a remaining player, mark them as the winner
@@ -88,37 +89,60 @@ class GameConsumer(AsyncWebsocketConsumer):
                         create_date=room.create_date,
                         end_date=room.end_date,
                     )
-
+                    logger.error(f"{self.user_id} Oyun verileri kayit edildi.")
                     # Delete the room from TMPGameDB
+                    logger.error(f"{room.room_id} oda silindi.")
                     await sync_to_async(room.delete)()
-
                     logger.error(
-                        f"Kullanıcı {self.user_id} odadan ayrıldı. Kazanan: {remaining_player_id}"
+                        f"Kullanici {self.user_id} odadan ayrildi. Kazanan: {remaining_player_id}"
                     )
                 else:
                     logger.error(
-                        f"Kullanıcı {self.user_id} odadan ayrıldı, Kazanan olmadı çünkü rakip yoktu."
+                        f"Kullanici {self.user_id} odadan ayrildi, Kazanan olmadi çünkü rakip yoktu."
                     )
+                    logger.error(f"{room.room_id} oda silindi.")
+                    await sync_to_async(room.delete)()
+            else:
+                logger.error(f"Websocket disconnect olurken {self.room_id} bulunamadi")
         except Exception as e:
             logger.error(f"disconnect metodunda hata: {e}")
 
-    # Gruptan gelen mesajı kullanıcıya ilet
+    # Gruptan gelen mesaji kullaniciya ilet
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["send"]
-        await self.send_msg(
-            msg=f"mesajini aldim bro raat ol ({message})"
-        )  # debug amaclıdır silinmeli
+        try:
+            if "type" not in text_data_json:
+                await self.send(
+                    json.dumps(
+                        {
+                            "status": "error",
+                            "message": "There must be type key as json format",
+                        }
+                    )
+                )
+            else:
+                type = text_data_json["type"]
+                if type == "ping":
+                    await self.send(
+                        json.dumps(
+                            {
+                                "status": "success",
+                                "message": "pong",
+                            }
+                        )
+                    )
+        except Exception as e:
+            logger.error(f"receive Error :{e}")
 
-    # Mesajı gruba gönder
+    # Mesaji gruba gönder
     async def send_msg(self, msg):
         await self.channel_layer.group_send(
             self.group_name, {"type": "group_message", "message": msg}
         )
 
-    # Grup mesajını işle
+    # Grup mesajini işle
     async def group_message(self, event):
         message = event["message"]
 
-        # Mesajı WebSocket üzerinden gönder
+        # Mesaji WebSocket üzerinden gönder
         await self.send(json.dumps({"message": message}))
